d7f0ac782689820c1f472d64b2ac8b89957cb961
Cameron Elliott <cameron.bryce.elliott@gmail.com>
2020-02-07 13:00:35 -0700

    Updated Wizard Wars Log In window

--- apps/wizard-wars/app.js
+++ apps/wizard-wars/app.js
@@ -2,33 +2,56 @@ startGame();
 var userName = "";
 var userPassword = "";
 function startGame() {
-	lblLogin.innerText="Username:";
+	lblLogin.innerText="Log In";
 	tbLogin.value="";
 	userName = "";
 	userPassword="";
 	tbLogin.focus();
 }
 
-function logIn() {
-	if (lblLogin.innerText=="Username:" && tbLogin.value == "")
-		alert("Please input a Username");
-	if (lblLogin.innerText=="Password:" && tbLogin.value == "")
-		alert("Please input a Password");
-	else if (lblLogin.innerText=="Username:" && tbLogin.value!="")
+let log = console.log;
+document.addEventListener('DOMContentLoaded', init);
+
+function init(){
+	let textLogIn = document.getElementById('tbLogin');
+	textLogIn.addEventListener('keyPress', checkKeyPress);
+}
+
+function checkKeyPress(ev){
+	let target = ev.currentTarget;
+	let tag = target.tagName;
+	let char = ev.char || ev. charCode || ev.which;
+	let s = String.fromCharCode(char);
+	log(char, s, tag);
+}
+
+function getUserInfo() {
+	if (tbLogin.placeholder == "Username" && tbLogin.value == "")
+		alert("Please enter a Username");
+	if (tbLogin.placeholder == "Password" && tbLogin.value == "")
+		alert("Please enter a Password");
+	else if (tbLogin.placeholder == "Username" && tbLogin.value!="")
 	{
 		userName=tbLogin.value;
 		tbLogin.value="";
 		tbLogin.type = "password";
-		lblLogin.innerText = "Password:";
+		lblLogin.innerText = userName;
+		tbLogin.placeholder = "Password";
 	}
-	else if (lblLogin.innerText=="Password:" && tbLogin.value!="")
+	else if (tbLogin.placeholder == "Password" && tbLogin.value!="")
 	{
 		userPassword=tbLogin.value;
 		tbLogin.value="";
 		tbLogin.type = "text";
-		lblLogin.innerText = "Username:";
+		tbLogin.placeholder = "Username";
 		alert("(test) Your password is " + userPassword);
+		//check database for user info. If user data is correct: logIn.
+		logIn();
 	}
-
 	tbLogin.focus();
+}
+
+function logIn()
+{
+	//loginWindow.mainContent.hide();
 }
