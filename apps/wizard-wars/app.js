2d0fc285d64e79ac87da160437c49e1ffd2ce585
Cameron Elliott <cameron.bryce.elliott@gmail.com>
2020-02-07 18:22:40 -0700

    welcome to wizard wars (log in)

--- apps/wizard-wars/app.js
+++ apps/wizard-wars/app.js
@@ -2,6 +2,8 @@ startGame();
 var userName = "";
 var userPassword = "";
 function startGame() {
+	gameLayout.style.display = "none";
+	loginWindow.style.display = "block";
 	lblLogin.innerText = "Log In";
 	tbLogin.value = "";
 	userName = "";
@@ -22,7 +24,7 @@ function checkKeyPress(ev){
 	let tag = target.tagName;
 	let char = ev.char || ev. charCode || ev.which;
 	let s = String.fromCharCode(char);
-	log(char, s, tag);
+	//log(char, s, tag);
 	switch (char) {
 		case 13: //enter
 			btnLogIn.click();
@@ -52,10 +54,11 @@ function getUserInfo() {
 	else if (tbLogin.placeholder == "Password" && tbLogin.value!="")
 	{
 		userPassword=tbLogin.value;
+		lblLogin.innerText="Log In";
 		tbLogin.value="";
 		tbLogin.type = "text";
 		tbLogin.placeholder = "Username";
-		log("Password: " + userPassword);
+		//log("Password: " + userPassword);
 		//check database for user info. If user data is correct: logIn.
 		logIn();
 	}
@@ -64,5 +67,8 @@ function getUserInfo() {
 
 function logIn()
 {
-	//loginWindow.mainContent.hide();
+	log("log in.");
+	welcome.innerText +=" "+userName;
+	loginWindow.style.display = "none";
+	gameLayout.style.display = "block";
 }
